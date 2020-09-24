7944f49c823125b1790680c30dbda5ec3a0f0e32
sairohithpasupuleti <47001724+sairohithpasupuleti@users.noreply.github.com>
2020-09-24 21:33:00 +0530

    Building lightning web components trail

---
+++ workingWithDataInLWC/force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,20 @@
+import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
+import FirstName_FIELD from '@salesforce/schema/Contact.FirstName';
+import LastName_FIELD from '@salesforce/schema/Contact.LastName';
+import Email_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+const COLUMNS = [
+    { label: 'Account FirstName', fieldName: FirstName_FIELD.fieldApiName, type: 'text' },
+    { label: 'LastName', fieldName: LastName_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: Email_FIELD.fieldApiName, type: 'text' }
+];
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+    get errors() {
+        return (this.accounts.error) ?
+            reduceErrors(this.accounts.error) : [];
+    }
+}
